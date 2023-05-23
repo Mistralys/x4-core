d9e520983c3ebcc020c9ee80991334ba34ff9ac4
Mistralys <s.mordziol@gmail.com>
2023-05-23 18:22:40 +0200

    Added unit test config and bootstrap config.

---
+++ tests/tests-config.dist.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Webserver URL to access the example UI under
+ * <code>/tests/X4Tests/ExampleUI</code>. Required
+ * to make the UI fully functional.
+ *
+ * Must be an absolute URL to the folder.
+ */
+const X4TESTS_INSTALL_URL = '';
