3e9b486aef532095558531eaba6513836c05ff5e
Mistralys <s.mordziol@gmail.com>
2024-05-02 18:07:36 +0200

    Added try blocks.

--- bin/extract-translations.php
+++ bin/extract-translations.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 
 namespace Mistralys\X4;
 
+use AppUtils\BaseException;
 use AppUtils\FileHelper\FolderInfo;
 use Mistralys\X4\Database\Translations\TranslationExtractor;
+use Mistralys\X4\UI\UserInterface;
 
 require_once __DIR__.'/prepend.php';
 
-$extractor = new TranslationExtractor(FolderInfo::factory(X4_EXTRACTED_CAT_FILES_FOLDER.'/t'));
-$extractor->selectLanguage(TranslationExtractor::LANGUAGE_ENGLISH);
-$extractor->extract();
+header('Content-Type: text/plain');
+
+try
+{
+    $extractor = new TranslationExtractor(FolderInfo::factory(X4_EXTRACTED_CAT_FILES_FOLDER . '/t'));
+    $extractor->selectLanguage(TranslationExtractor::LANGUAGE_ENGLISH);
+    $extractor->extract();
+}
+catch (BaseException $e)
+{
+    UserInterface::displayException($e);
+}
