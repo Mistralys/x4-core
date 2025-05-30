3c470f84eab9118613b780b40d3776a645a7ee82
Mistralys <s.mordziol@gmail.com>
2025-05-30 20:19:24 +0200

    Added new config constants.

--- dev-config.dist.php
+++ dev-config.dist.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace Mistralys\X4;
 
 /**
+ * Absolute path to the X4 game folder.
+ */
+const X4_GAME_FOLDER = '/path/to/x4';
+
+/**
  * Absolute path to the folder where the extracted
  * game files are stored (after unpacking them using
  * the XRCatTool).
@@ -13,3 +18,11 @@ namespace Mistralys\X4;
  * parts of the package.
  */
 const X4_EXTRACTED_CAT_FILES_FOLDER = '/path/to/folder';
+
+/**
+ * Used only to run the tests.
+ *
+ * This is the URL in the local webserver where
+ * the X4 Core library clone is installed.
+ */
+const X4_CORE_INSTALL_URL = 'http://127.0.0.1/tools/x4-core';
