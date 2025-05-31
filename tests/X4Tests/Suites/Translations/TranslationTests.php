a7ae4bc79f8b52c0dd7e1750ae552ded4c6ba1d2
Mistralys <s.mordziol@gmail.com>
2025-05-31 18:57:32 +0200

    Improved test.

--- tests/X4Tests/Suites/Translations/TranslationTests.php
+++ tests/X4Tests/Suites/Translations/TranslationTests.php
@@ -12,18 +12,6 @@ use const Mistralys\X4\X4_EXTRACTED_CAT_FILES_FOLDER;
 
 final class TranslationTests extends X4TestCase
 {
-    public function test_extract() : void
-    {
-        $extractor = new TranslationExtractor(FolderInfo::factory(X4_EXTRACTED_CAT_FILES_FOLDER . '/t'));
-
-        if(!$extractor->langExists(TranslationExtractor::LANGUAGE_ENGLISH)) {
-            $extractor->selectLanguage(TranslationExtractor::LANGUAGE_ENGLISH);
-            $extractor->extract();
-        }
-
-        $this->addToAssertionCount(1);
-    }
-
     public function test_translate() : void
     {
         $testID = '{20101,20604}';
@@ -31,5 +19,12 @@ final class TranslationTests extends X4TestCase
         $en = new TranslationDefs(TranslationExtractor::LANGUAGE_ENGLISH);
 
         $this->assertSame('Manorina (Gas) Vanguard', $en->ts($testID));
+    }
+
+    public static function setUpBeforeClass(): void
+    {
+        $extractor = new TranslationExtractor(FolderInfo::factory(X4_EXTRACTED_CAT_FILES_FOLDER . '/t'));
+        $extractor->selectLanguage(TranslationExtractor::LANGUAGE_ENGLISH);
+        $extractor->extract();
     }
 }
