950f4eed42a88de6c27342fa8161a09f76fe5f83
Mistralys <s.mordziol@gmail.com>
2025-06-06 11:47:42 +0200

    Updated constructor.

--- bin/extract-translations.php
+++ bin/extract-translations.php
@@ -7,6 +7,7 @@ namespace Mistralys\X4;
 use AppUtils\BaseException;
 use AppUtils\FileHelper\FolderInfo;
 use Mistralys\X4\Database\Translations\TranslationExtractor;
+use Mistralys\X4\ExtractedData\DataFolders;
 use Mistralys\X4\UI\UserInterface;
 
 require_once __DIR__.'/prepend.php';
@@ -15,7 +16,7 @@ header('Content-Type: text/plain');
 
 try
 {
-    $extractor = new TranslationExtractor(FolderInfo::factory(X4_EXTRACTED_CAT_FILES_FOLDER . '/t'));
+    $extractor = new TranslationExtractor(DataFolders::create(FolderInfo::factory(X4_EXTRACTED_CAT_FILES_FOLDER)));
     $extractor->selectLanguage(TranslationExtractor::LANGUAGE_ENGLISH);
     $extractor->extract();
 }
