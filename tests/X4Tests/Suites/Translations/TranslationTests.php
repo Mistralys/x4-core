accf76e9b8af4b15f60295e615f6039c411eaa4f
Mistralys <s.mordziol@gmail.com>
2025-06-07 15:06:56 +0200

    Fixed the test.

--- tests/X4Tests/Suites/Translations/TranslationTests.php
+++ tests/X4Tests/Suites/Translations/TranslationTests.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace X4Tests\Suites\Translations;
 
-use AppUtils\FileHelper\FolderInfo;
+use Mistralys\X4\Database\DatabaseBuilder;
+use Mistralys\X4\Database\Translations\Languages;
 use Mistralys\X4\Database\Translations\TranslationDefs;
 use Mistralys\X4\Database\Translations\TranslationExtractor;
 use X4Tests\Helpers\X4TestCase;
-use const Mistralys\X4\X4_EXTRACTED_CAT_FILES_FOLDER;
 
 final class TranslationTests extends X4TestCase
 {
@@ -16,15 +16,15 @@ final class TranslationTests extends X4TestCase
     {
         $testID = '{20101,20604}';
 
-        $en = new TranslationDefs(TranslationExtractor::LANGUAGE_ENGLISH);
+        $en = new TranslationDefs(Languages::LANGUAGE_ENGLISH);
 
         $this->assertSame('Manorina (Gas) Vanguard', $en->ts($testID));
     }
 
     public static function setUpBeforeClass(): void
     {
-        $extractor = new TranslationExtractor(FolderInfo::factory(X4_EXTRACTED_CAT_FILES_FOLDER . '/t'));
-        $extractor->selectLanguage(TranslationExtractor::LANGUAGE_ENGLISH);
+        $extractor = new TranslationExtractor(DatabaseBuilder::getDataFolders());
+        $extractor->selectLanguage(Languages::LANGUAGE_ENGLISH);
         $extractor->extract();
     }
 }
