3614e9d25bc5aa1917bd06ac7808eecec94fa45d
Mistralys <s.mordziol@gmail.com>
2025-06-11 22:12:43 +0200

    Added extraction command methods.

--- src/X4/Database/DatabaseBuilder.php
+++ src/X4/Database/DatabaseBuilder.php
@@ -1,16 +1,34 @@
 <?php
+/**
+ * @package X4 Database
+ * @subpackage Build Tools
+ */
 
 declare(strict_types=1);
 
 namespace Mistralys\X4\Database;
 
 use AppUtils\FileHelper\FolderInfo;
+use Mistral\X4\Database\Blueprints\BlueprintExtractor;
+use Mistralys\X4\Database\Factions\FactionsExtractor;
+use Mistral\X4\Database\Wares\WaresExtractor;
+use Mistralys\X4\Database\DataSources\DataSourceDef;
+use Mistralys\X4\Database\DataSources\DataSourceDefs;
+use Mistralys\X4\Database\MacroIndex\MacroIndexExtractor;
 use Mistralys\X4\Database\Modules\ModuleExtractor;
+use Mistralys\X4\Database\Ships\ShipsExtractor;
 use Mistralys\X4\Database\Translations\Languages;
 use Mistralys\X4\Database\Translations\TranslationExtractor;
 use Mistralys\X4\ExtractedData\DataFolders;
 use const Mistralys\X4\X4_EXTRACTED_CAT_FILES_FOLDER;
 
+/**
+ * Command endpoint for the Composer script commands
+ * used to build the database.
+ *
+ * @package X4 Database
+ * @subpackage Build Tools
+ */
 class DatabaseBuilder
 {
     private static bool $initialized = false;
@@ -30,6 +48,27 @@ class DatabaseBuilder
         }
 
         require_once $autoloader;
+    }
+
+    public static function extractBlueprints() : void
+    {
+        self::init();
+
+        (new BlueprintExtractor(self::getDataFolders()))->extract();
+    }
+
+    public static function extractWares() : void
+    {
+        self::init();
+
+        (new WaresExtractor(self::getDataFolders()))->extract();
+    }
+
+    public static function extractFactions() : void
+    {
+        self::init();
+
+        (new FactionsExtractor(self::getDataFolders()))->extract();
     }
 
     public static function extractTranslations() : void
@@ -54,8 +93,47 @@ class DatabaseBuilder
 
     public static function build() : void
     {
+        // Prerequisites
+        self::extractMacroIndex();
         self::extractTranslations();
+        self::writeDataSources();
+        self::extractFactions();
+        self::extractWares();
+
+        // Based on the wares
         self::extractModules();
+        self::extractBlueprints();
+        self::extractShips();
+    }
+
+    public static function extractShips() : void
+    {
+        self::init();
+
+        (new ShipsExtractor())->extract();
+    }
+
+    public static function extractMacroIndex() : void
+    {
+        self::init();
+
+        (new MacroIndexExtractor(self::getDataFolders()))->extract();
+    }
+
+    public static function writeDataSources() : void
+    {
+        self::init();
+
+        $data = array();
+        foreach(self::getDataFolders()->getAll() as $dataFolder) {
+            $data[] = DataSourceDef::toArray($dataFolder);
+        }
+
+        ksort($data);
+
+        DataSourceDefs::getInstance()
+            ->getDataFile()
+            ->putData($data);
     }
 
     private static ?DataFolders $dataFolders = null;
