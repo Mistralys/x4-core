2e897f4e345efba3fa801fd09b88f8278bb7266d
Mistralys <s.mordziol@gmail.com>
2025-06-13 22:29:25 +0200

    Added an interface for the item collections.

---
+++ src/X4/Database/Core/ItemCollectionInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\X4\Database\Core;
+
+use AppUtils\Interfaces\CollectionInterface;
+
+/**
+ * @method CollectionItemInterface getAll()
+ */
+interface ItemCollectionInterface extends CollectionInterface
+{
+
+}
