67411715c719b3e4c432c82c3728cb384f274a10
Mistralys <s.mordziol@gmail.com>
2025-06-13 20:40:09 +0200

    Added `findShips()`.

--- src/X4/Database/Ships/ShipDefs.php
+++ src/X4/Database/Ships/ShipDefs.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace Mistralys\X4\Database\Ships;
 
-
 use AppUtils\Collections\BaseStringPrimaryCollection;
 use AppUtils\FileHelper\JSONFile;
+use Mistralys\X4\Database\DataSources\DataSourceDef;
+use Mistralys\X4\Database\Factions\FactionDef;
+use Mistralys\X4\Database\Ships\Finder\ShipFinder;
 use Mistralys\X4\X4Application;
 
+/**
+ * @method ShipDef getByID(string $id)
+ * @method ShipDef[] getAll()
+ * @method ShipDef getDefault()
+ */
 class ShipDefs extends BaseStringPrimaryCollection
 {
     private static ?ShipDefs $instance = null;
@@ -40,10 +47,56 @@ class ShipDefs extends BaseStringPrimaryCollection
         return $this->dataFile;
     }
 
+    public function getFactions() : array
+    {
+        $result = array();
+
+        foreach($this->getAll() as $ship) {
+            $factionID = $ship->getBuilderFactionID();
+            if (!isset($result[$factionID])) {
+                $result[$factionID] = $ship->getBuilderFaction();
+            }
+        }
+
+        usort($result, function(FactionDef $a, FactionDef $b) : int {
+            return strnatcasecmp($a->getLabel(), $b->getLabel());
+        });
+
+        return array_values($result);
+    }
+
     protected function registerItems(): void
     {
         foreach($this->dataFile->getData() as $data) {
             $this->registerItem(ShipDef::fromArray($data));
         }
+    }
+
+    public function findShips() : ShipFinder
+    {
+        return new ShipFinder();
+    }
+
+    /**
+     * Returns a list of all data sources used by the ships in this collection.
+     * @return DataSourceDef[]
+     */
+    public function getDataSources() : array
+    {
+        $result = array();
+
+        foreach ($this->getAll() as $ship) {
+            $dataSourceID = $ship->getDataSourceID();
+            if (!isset($result[$dataSourceID])) {
+                $result[$dataSourceID] = $ship->getDataSource();
+            }
+        }
+
+        usort($result, function(DataSourceDef $a, DataSourceDef $b) : int {
+            return strnatcasecmp($a->getLabel(), $b->getLabel());
+        });
+
+        return $result;
+
     }
 }
