31e50a646ac5ea3c4cdc7b7103570aa73bcdb6af
Mistralys <s.mordziol@gmail.com>
2025-06-13 12:09:24 +0200

    Added ship variant handling.

--- src/X4/Database/Ships/ShipDef.php
+++ src/X4/Database/Ships/ShipDef.php
@@ -23,6 +23,8 @@ class ShipDef implements CollectionItemInterface
     public const KEY_CLASS_ID = 'classID';
     public const KEY_USED_BY = 'usedBy';
     public const KEY_DATA_SOURCE_ID = 'dataSourceID';
+    public const KEY_VARIANT_ID = 'variantID';
+    public const KEY_VARIANTS = 'variants';
 
     private string $id;
     private string $classID;
@@ -35,16 +37,35 @@ class ShipDef implements CollectionItemInterface
     private array $usedBy;
     private string $dataSourceID;
     private string $label;
+    private string $variantID;
 
-    public function __construct(string $id, string $label, string $size, string $builderFactionID, string $classID, array $usedBy, string $dataSourceID)
+    /**
+     * @var string[]
+     */
+    private array $variants;
+
+    /**
+     * @param string $id
+     * @param string $label
+     * @param string $variantID
+     * @param string $size
+     * @param string $builderFactionID
+     * @param string $classID
+     * @param array $usedBy
+     * @param string $dataSourceID
+     * @param string[] $variants IDs of this ship's variants, if any.
+     */
+    public function __construct(string $id, string $label, string $variantID, string $size, string $builderFactionID, string $classID, array $usedBy, string $dataSourceID, array $variants)
     {
         $this->id = $id;
         $this->label = $label;
+        $this->variantID = $variantID;
         $this->size = $size;
         $this->builderFactionID = $builderFactionID;
         $this->classID = $classID;
         $this->usedBy = $usedBy;
         $this->dataSourceID = $dataSourceID;
+        $this->variants = $variants;
     }
 
     public static function fromArray(array $def) : ShipDef
@@ -54,11 +75,13 @@ class ShipDef implements CollectionItemInterface
         return new self(
             $data->getString(self::KEY_WARE_ID),
             $data->getString(self::KEY_LABEL),
+            $data->getString(self::KEY_VARIANT_ID),
             $data->getString(self::KEY_SIZE),
             $data->getString(self::KEY_BUILDER_FACTION_ID),
             $data->getString(self::KEY_CLASS_ID),
             $data->getArray(self::KEY_USED_BY),
-            $data->getString(self::KEY_DATA_SOURCE_ID)
+            $data->getString(self::KEY_DATA_SOURCE_ID),
+            $data->getArrayFlavored(self::KEY_VARIANTS)->filterIndexedStrings()
         );
     }
 
@@ -75,6 +98,28 @@ class ShipDef implements CollectionItemInterface
     public function getSizeID(): string
     {
         return $this->size;
+    }
+
+    /**
+     * Returns the variant of the ship, which is used to differentiate between
+     * different models of the same ship type. An example of this is the
+     * Eclipse Vanguard, of which two versions exist with the exact same name:
+     *
+     * - ship_arg_s_heavyfighter_01_a
+     * - ship_arg_s_heavyfighter_02_a
+     *
+     * In this case, the variant IDs are "01-a" and "02-a" respectively.
+     *
+     * @return string
+     */
+    public function getVariantID(): string
+    {
+        return $this->variantID;
+    }
+
+    public function hasVariants() : bool
+    {
+        return !empty($this->variants);
     }
 
     public function getSize() : ShipSize
@@ -131,11 +176,13 @@ class ShipDef implements CollectionItemInterface
         return array(
             self::KEY_WARE_ID => $this->id,
             self::KEY_LABEL => $this->label,
+            self::KEY_VARIANT_ID => $this->variantID,
             self::KEY_SIZE => $this->size,
             self::KEY_BUILDER_FACTION_ID => $this->builderFactionID,
             self::KEY_CLASS_ID => $this->classID,
             self::KEY_USED_BY => $this->usedBy,
-            self::KEY_DATA_SOURCE_ID => $this->dataSourceID
+            self::KEY_DATA_SOURCE_ID => $this->dataSourceID,
+            self::KEY_VARIANTS => $this->variants
         );
     }
 }
