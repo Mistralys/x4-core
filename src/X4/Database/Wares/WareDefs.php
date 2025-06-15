54c86cca6d63a97f5d596dfe9deb29a0af826aef
Mistralys <s.mordziol@gmail.com>
2025-06-15 10:34:17 +0200

    Added `findByMacro()`.

--- src/X4/Database/Wares/WareDefs.php
+++ src/X4/Database/Wares/WareDefs.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Mistralys\X4\Database\Wares;
 
 use AppUtils\Collections\BaseStringPrimaryCollection;
+use AppUtils\FileHelper;
 use AppUtils\FileHelper\JSONFile;
 use Mistral\X4\Database\Wares\WaresExtractor;
 use Mistralys\X4\Database\Core\ItemCollectionInterface;
@@ -95,5 +96,51 @@ class WareDefs extends BaseStringPrimaryCollection implements ItemCollectionInte
     public function getDataFile() : JSONFile
     {
         return $this->dataFile;
+    }
+
+    /**
+     * Attempts to find a ware definition by its ware ID, macro ID or macro file name.
+     * @param string $macroNameOrFile E.g. `ship_arg_l_trans_container_04_a` (ware ID), `ship_arg_l_trans_container_04_a_macro` (macro ID) or `ship_arg_l_trans_container_04_a_macro.xml`.
+     * @return WareDef|null
+     */
+    public function findByMacro(string $macroNameOrFile) : ?WareDef
+    {
+        if(str_contains($macroNameOrFile, '.')) {
+            $macroName = FileHelper::removeExtension($macroNameOrFile);
+        } else {
+            $macroName = $macroNameOrFile;
+        }
+
+        $index = $this->getMacroIndex();
+
+        return $index[$macroName] ?? null;
+    }
+
+    /**
+     * @return array<string, WareDef>
+     */
+    private ?array $macroIndex = null;
+
+    /**
+     * Returns an index of all wares by their ware ID and macro ID.
+     * This allows for quick lookups by either ID. Also see {@see self::findByMacro()},
+     * which uses this index.
+     *
+     * @return array<string, WareDef>
+     */
+    public function getMacroIndex() : array
+    {
+        if(isset($this->macroIndex)) {
+            return $this->macroIndex;
+        }
+
+        $this->macroIndex = array();
+
+        foreach($this->getAll() as $ware) {
+            $this->macroIndex[$ware->getWareID()] = $ware;
+            $this->macroIndex[$ware->getMacroID()] = $ware;
+        }
+
+        return $this->macroIndex;
     }
 }
