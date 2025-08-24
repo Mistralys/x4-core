096cb1a1b8aa2c54cd455b17bb884b8e8ccf47af
Mistralys <s.mordziol@gmail.com>
2025-08-24 12:05:02 +0200

    Use the main dev-config file.

--- tests/X4Tests/ExampleUI/index.php
+++ tests/X4Tests/ExampleUI/index.php
@@ -8,7 +8,15 @@ declare(strict_types=1);
 
 use X4Tests\Helpers\X4TestCase;
 
+if(!file_exists(__DIR__.'/../../../vendor/autoload.php')) {
+    die('Please run "composer install" to set up the test environment.');
+}
+
+if(!file_exists(__DIR__.'/../../../dev-config.php')) {
+    die('Please create a "dev-config.php" file based on "dev-config.dist.php".');
+}
+
 require_once __DIR__.'/../../../vendor/autoload.php';
-require_once __DIR__.'/config.php';
+require_once __DIR__.'/../../../dev-config.php';
 
 X4TestCase::createTestApp()->getUI()->display();
